82b5f661d83c8bc2b6f91414cc91c3b378df58db
Ivan Krešić <ik017703661@fer.hr>
2016-06-25 15:00:08 +0200

    frontend

---
+++ WTH_ik/platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,28 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-bluetooth-serial/www/bluetoothSerial.js",
+        "id": "cordova-plugin-bluetooth-serial.bluetoothSerial",
+        "pluginId": "cordova-plugin-bluetooth-serial",
+        "clobbers": [
+            "window.bluetoothSerial"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-bluetoothle/www/bluetoothle.js",
+        "id": "cordova-plugin-bluetoothle.BluetoothLe",
+        "pluginId": "cordova-plugin-bluetoothle",
+        "clobbers": [
+            "window.bluetoothle"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-bluetooth-serial": "0.4.5",
+    "cordova-plugin-bluetoothle": "4.0.0"
+}
+// BOTTOM OF METADATA
+});
